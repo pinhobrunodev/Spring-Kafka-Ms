8ceb5f21767c3084cf0f8584e8cfab076dd7985b
pinhobrunodev <iaishowcs@gmail.com>
2022-01-22 17:38:40 -0300

    fix

--- course/src/main/java/com/pinhobrunodev/course/consumers/KafkaListening.java
+++ course/src/main/java/com/pinhobrunodev/course/consumers/KafkaListening.java
@@ -2,7 +2,6 @@ package com.pinhobrunodev.course.consumers;
 
 import com.pinhobrunodev.course.dtos.UserConsumerDto;
 import com.pinhobrunodev.course.enums.ActionType;
-import com.pinhobrunodev.course.models.UserModel;
 import com.pinhobrunodev.course.services.UserService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +9,8 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Log4j2
 @Component
@@ -21,13 +22,14 @@ public class KafkaListening {
 
 
     @KafkaListener(topics = "user-topic", groupId = "group-1", containerFactory = "jsonKafkaListenerContainerFactory")
-    public void createUser(UserConsumerDto userConsumerDto,
+    public void userListen(List<UserConsumerDto> userConsumerDtos,
                            @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
+        var actionType = userConsumerDtos.stream().findFirst().get().getActionType();
+        var userModel = userConsumerDtos.stream().findFirst().map(x -> x.convertToUserModel()).orElseThrow(() -> new RuntimeException("Error during converting Dto to Model"));
         log.info("Partition : {} ", partition);
         log.info("Thread: {}", Thread.currentThread().getId());
-        log.info("Message received : {} ",userConsumerDto.toString());
-        var userModel = userConsumerDto.convertToUserModel();
-        switch (ActionType.valueOf(userConsumerDto.getActionType())) {
+        log.info("Message received : {} ", userConsumerDtos.toString());
+        switch (ActionType.valueOf(actionType)) {
             case CREATE:
             case UPDATE:
                 userService.saveUser(userModel);
